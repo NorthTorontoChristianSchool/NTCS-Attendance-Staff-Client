cb8c6d4412d1e59f51641702360fc3cddbbf0827
fffelix-jan <fffelix.jan.yt@gmail.com>
2021-06-25 13:47:56 -0400

    Create SqlConnectionInfo.cs

---
+++ SqlConnectionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+using System.Security;
+
+namespace NTCSAttendanceStaffClient
+{
+    public static class SqlConnectionInfo
+    {
+        public static SecureString SecurePassword { get; set; }
+        public static SqlCredential Credentials { get; set; }
+        public static string ConnectionString { get; set; }
+    }
+}
